8b68333957478b402ae3edf1c7822b9a5d0cea2e
pranish <pranishshrestha181@gmail.com>
2022-05-03 12:42:24 +0545

    feta:input date of birth and lucky number to check

---
+++ script.js
@@ -0,0 +1,9 @@
+const yourBirthday = document.querySelector("#birthday");
+const luckyNumber =document.querySelector('#lucky-number');
+const checkButton =document.querySelector('#check');
+const outputBox = document.querySelector('#output-box');
+checkButton.addEventListener('click',function (){
+
+});
+
+
